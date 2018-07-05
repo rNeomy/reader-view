ab9bbc6fa38118f40c0d16a9b759589dda3aea12
greatghoul <greatghoul@gmail.com>
2018-07-05 11:02:05 +0800

    Focus the iframe content window.
    Make the iframe focus after reader loaded,
    so users can use `Space` key to scroll the page down.

--- WebExtension/data/reader/index.js
+++ WebExtension/data/reader/index.js
@@ -69,6 +69,7 @@ body {
     iframe.contentDocument.body.dataset.mode = prefs.mode;
     document.body.dataset.mode = prefs.mode;
     styles.textContent = prefs['user-css'];
+    iframe.contentWindow.focus();
   });
 }
 
