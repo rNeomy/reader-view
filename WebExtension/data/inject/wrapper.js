36b98ea61349df856cacc937c7e1eb454b638031
greatghoul <greatghoul@gmail.com>
2018-07-20 02:15:20 +0800

    Selected content as article body
    Sometimes the readability parser can not grab the correct content of an article,
    users can selected what they want to read, then read it in Reader View

--- WebExtension/data/inject/wrapper.js
+++ WebExtension/data/inject/wrapper.js
@@ -1,7 +1,45 @@
 /* globals Readability */
 'use strict';
 
-var article = new Readability(document.cloneNode(true)).parse();
+// The implementation is from https://stackoverflow.com/a/5084441/260793
+function getSelectionHTML() {
+  var userSelection;
+  if (window.getSelection) {
+    // W3C Ranges
+    userSelection = window.getSelection();
+    if (!userSelection.rangeCount) {
+       return '';
+    }
+
+    // Get the range:
+    if (userSelection.getRangeAt)
+      var range = userSelection.getRangeAt(0);
+    else {
+      var range = document.createRange();
+      range.setStart(userSelection.anchorNode, userSelection.anchorOffset);
+      range.setEnd(userSelection.focusNode, userSelection.focusOffset);
+    }
+    // And the HTML:
+    var clonedSelection = range.cloneContents();
+    var div = document.createElement('div');
+    div.appendChild(clonedSelection);
+    return div.innerHTML;
+  } else if (document.selection) {
+    // Explorer selection, return the HTML
+    userSelection = document.selection.createRange();
+    return userSelection.htmlText;
+  } else {
+    return '';
+  }
+}
+
+var doc = document.cloneNode(true);
+var content = getSelectionHTML().replace(/(^\s+|\s+$)/mg, '');
+if (content) {
+  doc.body.innerHTML = content;
+}
+
+var article = new Readability(doc).parse();
 
 // if a website has an automatic redirect use this method to wait for a new page load
 if (location.href.indexOf('://news.google.') !== -1 &&
