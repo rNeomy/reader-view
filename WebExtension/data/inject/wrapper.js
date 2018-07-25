b4f52aad988c793e080a98c14d36161cc1260dfb
greatghoul <greatghoul@gmail.com>
2018-07-26 07:56:10 +0800

    Improve getSelectionHTML

--- WebExtension/data/inject/wrapper.js
+++ WebExtension/data/inject/wrapper.js
@@ -3,31 +3,20 @@
 
 // The implementation is from https://stackoverflow.com/a/5084441/260793
 function getSelectionHTML() {
-  var userSelection;
-  if (window.getSelection) {
-    // W3C Ranges
-    userSelection = window.getSelection();
-    if (!userSelection.rangeCount) {
-       return '';
-    }
-
-    // Get the range:
-    if (userSelection.getRangeAt)
-      var range = userSelection.getRangeAt(0);
-    else {
-      var range = document.createRange();
+  var userSelection = window.getSelection();
+  if (userSelection && userSelection.rangeCount) {
+    var range;
+    if (userSelection.getRangeAt) {
+      range = userSelection.getRangeAt(0);
+    } else {
+      range = document.createRange();
       range.setStart(userSelection.anchorNode, userSelection.anchorOffset);
       range.setEnd(userSelection.focusNode, userSelection.focusOffset);
     }
-    // And the HTML:
-    var clonedSelection = range.cloneContents();
+
     var div = document.createElement('div');
-    div.appendChild(clonedSelection);
+    div.appendChild(range.cloneContents());
     return div.innerHTML;
-  } else if (document.selection) {
-    // Explorer selection, return the HTML
-    userSelection = document.selection.createRange();
-    return userSelection.htmlText;
   } else {
     return '';
   }
