37a38492553ceee57e8c94db792947b672d768d6
Richard Neomy <richard.neomy@openmailbox.org>
2020-12-19 12:30:47 +0100

    fixes #87

--- WebExtension/firefox/data/inject/wrapper.js
+++ WebExtension/firefox/data/inject/wrapper.js
@@ -75,7 +75,7 @@
 // The implementation is from https://stackoverflow.com/a/5084441/260793
 function getSelectionHTML() {
   const selection = window.getSelection();
-  if (selection && selection.rangeCount) {
+  if (selection && selection.rangeCount && selection.toString().trim().length > 20) {
     let range;
     if (selection.getRangeAt) {
       range = selection.getRangeAt(0);
